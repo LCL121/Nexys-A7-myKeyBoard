0b3f01886dc0d72dc449843c290963bc9d42c6dd
LCL121 <47299089+LCL121@users.noreply.github.com>
2019-12-06 20:04:22 +0800

    change

--- myKeyBoard.srcs/sim_1/new/myKeyboardSim.v
+++ myKeyBoard.srcs/sim_1/new/myKeyboardSim.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2019/11/27 22:42:03
-// Design Name: 
-// Module Name: myKeyboardSim
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module myKeyboardSim;
 
